5e80252f3babadd9b4e2c10bba4451a8ab10dc2f
Vicky Kumar <vimleshvki@hotmail.com>
2022-12-13 18:09:54 +0530

    initialcommit

---
+++ vkicore/Controllers/SecurityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vkicore.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+
+}
